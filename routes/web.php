684aa267cfbfb595529a772a53a20545be7d2f55
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-02-16 22:26:38 +0900

    Create follow button

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 //ユーザー関連
 Route::prefix('users')->name('users.')->group(function () {
   Route::get('/{name}', 'UserController@show')->name('show');
+  Route::middleware('auth')->group(function () {
+    Route::put('/{name}/follow', 'UserController@follow')->name('follow');
+    Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
+});
 });
